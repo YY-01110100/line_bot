7010906a10230e0a6b614a201c47cd35591171b0
Adam Zhang <q360465913@gmail.com>
2019-02-10 22:00:34 -0800

    update

--- app.py
+++ app.py
@@ -37,7 +37,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃饭了吗'
+    r = '我看不懂'
+
+    if msg == 'hi'：
+        r = 'hi'
+    elif msg == '你吃饭了么'
+        r = '还没'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
